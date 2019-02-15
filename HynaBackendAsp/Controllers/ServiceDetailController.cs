1618ac9c0fa8c2e75cb4840b5a8b4a66c0b8a982
Sabina Aqashova <aqashova@inbox.ru>
2019-02-15 23:11:17 +0400

    AddedNewControllers

---
+++ HynaBackendAsp/Controllers/ServiceDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HynaBackendAsp.Controllers
+{
+    public class ServiceDetailController : Controller
+    {
+        // GET: ServiceDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
